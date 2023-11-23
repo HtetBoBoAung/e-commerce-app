1a5e3f6ef01b95f6a2fe29969ca2b40d4680f3d4
Htet Bo Bo Aung <boboa3473@gmail.com>
2023-11-23 21:19:30 +0630

    Create The Form

--- index.js
+++ index.js
@@ -2,41 +2,37 @@
 //login page
 //login api function 
 //local storage save 
+
 async function postLogIn(name, password) {
      try {
           const postOption = {
                username: name,
                password: password,
           };
+          
 
           const response = await axios.post("https://fakestoreapi.com/auth/login", postOption);
-          console.log(response.data);
+          console.log(response);
      } catch (err) {
           console.error(err);
      }
 }
+postLogIn(email, password);
 
-const username = "mor_2314";
-const password = "83r5^_";
-postLogIn(username, password);
+const signupBtn = document.getElementById("signup");
+const loginBtn = document.getElementById("login");
 
-//
+
 const submitBtn = document.getElementById("submitBtn");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("password");
-
+const form = document.querySelector(".form");
 const arrays = [];
-console.log(arrays);
 
 
-function displayfunction() {
-     let userInput = userEmail.value;
-     let user_Password = userPassword.value;
-     console.log(user_Password);
-     arrays.push(userInput,user_Password);
-     localStorage.setItem("arrays", arrays);
-     const getLocalStorage = localStorage.getItem("arrays");
-     console.log(getLocalStorage);
-}
-
-submitBtn.addEventListener("click", displayfunction);
+form.addEventListener("submit" , (e) => {
+     let email = userEmail.value;
+     let password = userPassword.value;
+     console.log(email);
+     console.log(password);
+});
